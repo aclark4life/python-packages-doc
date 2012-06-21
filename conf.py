6b3871c35db3072df964648e76c011ad9661e1dc
Alex Clark <aclark@aclark.net>
2012-06-21 16:12:46 -0400

    Switch theme

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pythonpackages.com'
-copyright = u'2011, ACLARK.NET, LLC'
+copyright = u'2011-2012 ACLARK.NET, LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

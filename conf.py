612ae658440753fcb5728a24d5a137c04193369b
Alex Clark <aclark@aclark.net>
2012-07-15 21:14:08 -0400

    Add css

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pythonpackages.com'
-copyright = u'2011-2012 ACLARK.NET, LLC'
+copyright = u'©2012 ACLARK.NET, LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
